324ba7137abd4d86aed14447e4844ce365702f26
HyunHee <h2koo2004@gmail.com>
2025-04-21 09:34:12 +0900

    Initial commit

---
+++ src/main/java/com/study/jpa/JpaStudy202504Application.java
@@ -0,0 +1,13 @@
+package com.study.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudy202504Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaStudy202504Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/jpa/JpaStudy202504ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudy202504ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
